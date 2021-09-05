11c8a524f1e2cf8fcebe2d15cce924a7441039bc
DanielLucaci <46228296+DanielLucaci@users.noreply.github.com>
2021-09-05 16:51:34 +0300

    Updated Menu.js

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import CardContainer from './components/CardContainer';
 
 function App() {
+  const [playing, setPlaying] = useState(false); 
+  const [category, setCategory] = useState(false);
+  const [difficulty, setDifficulty] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {playing ? 
+       <CardContainer /> : 
+       <Menu 
+         difficulty={difficulty} 
+         category={category}
+         onChangeCategory={(category) => setCategory(category)}
+         onChangeDifficulty={(difficulty) => setDifficulty(difficulty)}
+         onPlayButton={() => setPlaying(true)}
+       />
+      }
+    </>
   );
 }
 
---
+++ card-game/src/components/Card.js
@@ -0,0 +1,10 @@
+
+
+const Card = (props) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export const Card;
